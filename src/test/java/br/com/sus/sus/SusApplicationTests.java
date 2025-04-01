a175086600bb1b169f5a9cd467ffaf04a4f5772c
marcelo <marcelomscode+mscodeblog@gmail.com>
2025-04-01 12:51:16 -0300

    first commit

---
+++ src/test/java/br/com/sus/sus/SusApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sus.sus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
